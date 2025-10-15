e6e80cb07bd202d1cb6276054872ed6b6a990389
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2025-10-15 12:10:04 +0100

    fixed mongoodb connect problem

--- src/app/api/user/updateUser/route.ts
+++ src/app/api/user/updateUser/route.ts
@@ -3,42 +3,36 @@ import { verifyToken } from "@/app/utils/middleware";
 import { NextRequest, NextResponse } from "next/server";
 import User from "../../model/user.model";
 
-
-
-type usertoUpdateType = {
+type UserToUpdateType = {
   firstname?: string;
   lastname?: string;
   email?: string;
-}
+};
+
 export const PATCH = async (req: NextRequest) => {
   try {
-
     await connectDB();
     const user = await verifyToken(req);
-    const { firstname, lastname, email}: usertoUpdateType = await req.json();
+    const { firstname, lastname, email }: UserToUpdateType = await req.json();
 
+    const userToUpdate: UserToUpdateType = {};
+    if (firstname) userToUpdate.firstname = firstname;
+    if (lastname) userToUpdate.lastname = lastname;
+    if (email) userToUpdate.email = email;
 
-    const usertoUpdate: usertoUpdateType = {}
+    const updatedUser = await User.findByIdAndUpdate(
+      user._id,
+      { ...userToUpdate }, // <-- spread here
+      { new: true }
+    ).select("-password");
 
-    if(firstname){
-      usertoUpdate.firstname = firstname
+    if (!updatedUser) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
     }
-    if(lastname){
-      usertoUpdate.lastname = lastname
-    }
-    if(email){
-      usertoUpdate.email = email
-    }
 
-   const updateUser = await User.findByIdAndUpdate(user._id, {
-    usertoUpdate
-   }, {new: true}).select("-password");
-
-   if(!updateUser) {
-    return NextResponse.json({ message: "User not found" }, { status: 404 });
-   }
-   return NextResponse.json({ user: updateUser }, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    return NextResponse.json({ user: updatedUser }, { status: 200 });
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : "Unauthorized";
+    return NextResponse.json({ message }, { status: 401 });
   }
 };
