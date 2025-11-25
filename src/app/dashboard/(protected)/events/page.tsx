776e0c106f6286c3a19f17efaf2db6ecee05fe05
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2025-11-25 10:20:45 +0100

    designed the event and create event button

--- src/app/dashboard/(protected)/events/page.tsx
+++ src/app/dashboard/(protected)/events/page.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Events from '.'
+import React from "react";
+import Events from ".";
 
 const page = () => {
   return (
-    <div> 
-      <h1>Events</h1>
-      <p>List of events will be displayed here.</p>
+    <div>
+      <div className="flex justify-between items-center mb-10">
+        <h1 className="text-2xl font-bold mb-4">Events Page</h1>
+        <button className="bg-amber-500 text-white px-4 py-2 rounded">
+          Create Event
+        </button>
+      </div>
       <Events />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
