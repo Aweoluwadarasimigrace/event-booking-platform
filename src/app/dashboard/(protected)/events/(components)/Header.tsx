5e7e7c1879d37915f26863332c9ba76af53cd97d
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2025-11-25 11:23:38 +0100

    adjusted the header code

--- src/app/dashboard/(protected)/events/(components)/Header.tsx
+++ src/app/dashboard/(protected)/events/(components)/Header.tsx
@@ -1,13 +1,22 @@
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const Header = () => {
+    const pathName = usePathname();
+    const headerItems = [
+        {item: "All Events", path: "/dashboard/events"},
+        {item: "ongoing events", path: "/dashboard/events/ongoing"},
+        {item: "ended events", path: "/dashboard/events/ended"},
+    ];
   return (
     <div>
         <div className='flex justify-between items-center mb-10 border-b pb-4 border-gray-300'>
-            <p><Link href={""}>All Events</Link></p>
-            <p><Link href={""}>ongoing events</Link></p>
-            <p><Link href={""}>ended events</Link></p>
+            {headerItems.map(({item, path}) => (
+                <p key={path} className={path === pathName ? 'font-bold' : ''}>
+                    <Link href={path}>{item}</Link>
+                </p>
+            ))}
         </div>
     </div>
   )
