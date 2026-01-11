0a3a249e9b7ceffa5d53e3d8e55ad94feb239563
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2026-01-11 11:44:42 +0100

    new changes

--- src/app/dashboard/(protected)/events/page.tsx
+++ src/app/dashboard/(protected)/events/page.tsx
@@ -9,7 +9,7 @@ const page = () => {
     <div>
       <div className="flex justify-between items-center mb-10">
         <h1 className="text-2xl font-bold mb-4">Events Page</h1>
-      <Link href="/dashboard/events/createevent" className="bg-[#fc6435] text-white px-4 py-2 rounded hover:bg-amber-100 hover:text-amber-500 font-semibold">Create Event</Link>
+        <Link href="/dashboard/events/createevent" className="inline-flex items-center justify-center bg-[#fc6435] text-white font-semibold px-3 py-2 text-sm sm:px-4 sm:py-2 sm:text-base md:px-5 md:py-2.5 rounded-lg hover:bg-amber-100 hover:text-amber-500 transition-colors duration-200 w-full sm:w-auto" > Create Event </Link>
       </div>
       <Header />
       <Events />
