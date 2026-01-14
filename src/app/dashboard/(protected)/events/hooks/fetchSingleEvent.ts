4533071f48d07a0f47a86a9d4c952c689745c62e
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2026-01-14 02:46:16 +0100

    trouble shooting display one page

--- src/app/dashboard/(protected)/events/hooks/fetchSingleEvent.ts
+++ src/app/dashboard/(protected)/events/hooks/fetchSingleEvent.ts
@@ -2,7 +2,9 @@ import { apiClient } from "@/app/lib/client";
 
 export const fetchSingleEvent = async (id: string) => {
   try {
+    console.log(id, "fetch id")
     const res = await apiClient.get(`/api/Event/${id}`);
+    console.log(res.data)
     return res.data.event;
   } catch (error) {
     console.error("Error fetching event data:", error);
