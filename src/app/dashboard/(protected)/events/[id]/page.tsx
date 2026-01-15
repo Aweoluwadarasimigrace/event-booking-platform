b85886223ebc0749ecf883ce2801640a8452c7c1
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2026-01-15 09:19:12 +0100

    new changes

--- src/app/dashboard/(protected)/events/[id]/page.tsx
+++ src/app/dashboard/(protected)/events/[id]/page.tsx
@@ -1,10 +1,10 @@
-import { ParamType } from '@/type'
+import { idType} from '@/type'
 import React from 'react'
 import { fetchSingleEvent } from '../hooks/fetchSingleEvent';
-import DisplaySingleEvent from '.';
+// import DisplaySingleEvent from '.';
 
-const Page = async ({params}: ParamType) => {
-    const { id } = await params;
+const Page = async({params}: idType) => {
+    const {id} = await params
     console.log(id)
     const event = await fetchSingleEvent(id);
   return (
--- src/type/index.ts
+++ src/type/index.ts
@@ -1,3 +1,7 @@
 export type ParamType = {
     params: Promise<{ id: string }>
 }
+
+export type idType = {
+  params: Promise<{ id: string }>;
+};
